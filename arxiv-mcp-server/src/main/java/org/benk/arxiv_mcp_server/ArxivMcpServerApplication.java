8f5c2605c8adc6dd4026488dc8c650344a2ba9f3
Veniamin Kalegin <veniamin.kalegin@infor.com>
2025-06-04 13:55:02 -0400

    bootstrap app

---
+++ arxiv-mcp-server/src/main/java/org/benk/arxiv_mcp_server/ArxivMcpServerApplication.java
@@ -0,0 +1,13 @@
+package org.benk.arxiv_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArxivMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArxivMcpServerApplication.class, args);
+	}
+
+}
---
+++ arxiv-mcp-server/src/test/java/org/benk/arxiv_mcp_server/ArxivMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.benk.arxiv_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArxivMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
